b874e47ee6c6e046d1ffb0469ce9c07bfa7edcc6
andriusmw <andriusmw@yahoo.es>
2022-02-22 15:59:28 +0100

    mostrando lista usuarios del mock-usuarios

---
+++ src/app/mock-usuarios.ts
@@ -0,0 +1,14 @@
+import { Usuario } from './usuario';
+
+export const USUARIOS: Usuario[] = [
+  { id: 11, name: 'Dr Nice', surname: 'Dr Nice'},
+  { id: 12, name: 'Narco', surname: "spider" },
+  { id: 13, name: 'Bombasto', surname: "oritrón" },
+  { id: 14, name: 'Celeritas', surname:"Dapillah" },
+  { id: 15, name: 'Magneta', surname: "Ontto" },
+  { id: 16, name: 'RubberMan', surname:" Ontta" },
+  { id: 17, name: 'Dynama' , surname:"Parker"},
+  { id: 18, name: 'Dr IQ', surname: "Williams" },
+  { id: 19, name: 'Magma', surname:"BA" },
+  { id: 20, name: 'Tornado',surname:"2" }
+];
--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../usuario';
+import { USUARIOS } from '../mock-usuarios';
 
 @Component({
   selector: 'app-usuarios',
@@ -8,11 +9,15 @@ import { Usuario } from '../usuario';
 })
 export class UsuariosComponent implements OnInit {
 
+  usuarios = USUARIOS;
+  //Creo una variable llamada usuarios y le paso el valor, contenido y tipo del array USUARIOS del mock mock-usuarios
+
   usuario : Usuario = {
     id: 1,
     name: 'Windstorm',
     surname: 'Snow'
   }
+  //Creo una variable llamada usuario, de tipo objeto que va a tener el contenido y estructura de Usuario. Que lo saco de la interfaz
 
   constructor() { }
 
