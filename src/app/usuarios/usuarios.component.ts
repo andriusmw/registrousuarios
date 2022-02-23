bf9abbd78586f27b16597084e6355fafc7c65f96
andriusmw <andriusmw@yahoo.es>
2022-02-23 14:47:23 +0100

    añadiendo obersvable data

--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../usuario';
-import { USUARIOS } from '../mock-usuarios';
+//import { USUARIOS } from '../mock-usuarios';
+import { UsuarioService } from '../usuario.service';
 
 @Component({
   selector: 'app-usuarios',
@@ -15,8 +16,10 @@ export class UsuariosComponent implements OnInit {
 
 
 
-  usuarios = USUARIOS;
+  //usuarios = USUARIOS;
   //Creo una variable llamada usuarios y le paso el valor, contenido y tipo del array USUARIOS del mock mock-usuarios
+  usuarios: Usuario[] = []
+  //Reemplazamos lo de la linea 19 por la linea 21 al Usar el servicio ahora en vez de el mock
 
   usuario : Usuario = {
     id: 1 ,
@@ -25,9 +28,17 @@ export class UsuariosComponent implements OnInit {
   }
   //Creo una variable llamada usuario, de tipo objeto que va a tener el contenido y estructura de Usuario. Que lo saco de la interfaz
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService) { }
+  //los servicios se declaran en el constructor
+
+  getUsuarios(): void {
+    this.usuarios = this.usuarioService.getUsuarios();
+  }
+  //Vuelve a declarar getUsuarios, para conectar con el servicio y que este conecte con el mock
 
   ngOnInit(): void {
+    this.getUsuarios();
+    //Lo llamamos al cargar para que llame al servicio, --> mock --> devuelve lista usuarios--> la muestra
   }
 
   onSelect(usuario: Usuario): void {
@@ -38,4 +49,8 @@ export class UsuariosComponent implements OnInit {
   //this-selectedUsuario = this.usuario ; eso por algún motivo no hacía nada, porque lo que queríamos era
   // pasar los valores de selectedUsuario a la variable usuario, y digamos que se lo estábamos pasando a otra variable distinta.
   //Por tema de scope
+
+
+
+
 }
