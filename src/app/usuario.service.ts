15befe8548351b05af95678b630bb9ccceff4768
andriusmw <andriusmw@yahoo.es>
2022-02-23 14:53:15 +0100

    cvonfigurando el mensaje

--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Usuario } from './usuario';
+import { USUARIOS } from './mock-usuarios';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+
+
+  getUsuarios(): Observable<Usuario[]> {
+    const usuarios = of(USUARIOS);
+    this.messageService.add('UsuarioService: fetched USUARIOS');
+    return usuarios;
+  }
+
+  constructor(private messageService: MessageService) { }
 }
