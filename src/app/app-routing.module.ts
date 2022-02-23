d2a4362218784dee21af0032aeeec816df1139a7
andriusmw <andriusmw@yahoo.es>
2022-02-23 15:27:41 +0100

    añadido routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'usuarios', component: UsuariosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
