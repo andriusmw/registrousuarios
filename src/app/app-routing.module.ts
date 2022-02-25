b1fde80e3f34f8d84e38f0bb03017656e302b5e5
andriusmw <andriusmw@yahoo.es>
2022-02-25 15:53:42 +0100

    añadido routing detectado que añade un usuario para el nombre y otro para el surname

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 
 const routes: Routes = [
   { path: 'usuarios', component: UsuariosComponent },
+  { path: 'adduser', component: AddUserComponent },
   { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
