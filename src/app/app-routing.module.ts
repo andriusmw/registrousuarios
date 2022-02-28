f5ff7d906c5fdb47a5dfb335287745062b20dd8a
andriusmw <andriusmw@yahoo.es>
2022-02-28 16:34:08 +0100

    mejorada organizacion de menus y vistas de cada parte del routing outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddUserComponent } from './add-user/add-user.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { UsuarioSearchComponent } from './usuario-search/usuario-search.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+
 
 const routes: Routes = [
   { path: 'usuarios', component: UsuariosComponent },
   { path: 'adduser', component: AddUserComponent },
   { path: 'detail/{{usuario.id}}', component: UserDetailsComponent },
+  { path: 'search', component: UsuarioSearchComponent },
   { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Registro de Usuarios';
+  title = 'PROGRAMA DE USUARIOS';
 
 
 
