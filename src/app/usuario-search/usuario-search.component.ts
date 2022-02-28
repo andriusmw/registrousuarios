a2753e9797c2536282bec44c0091fd9c8bb3db5f
andriusmw <andriusmw@yahoo.es>
2022-02-28 15:17:30 +0100

    componente search creado y funcional

--- src/app/usuario-search/usuario-search.component.ts
+++ src/app/usuario-search/usuario-search.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, Subject } from 'rxjs';
+
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
+import { AppRoutingModule } from '../app-routing.module';
+
+
+import { Usuario } from '../usuario';
+
+import { UsuarioService } from '../usuario.service';
+
+
 @Component({
   selector: 'app-usuario-search',
   templateUrl: './usuario-search.component.html',
-  styleUrls: ['./usuario-search.component.css']
+  styleUrls: [ './usuario-search.component.css' ]
 })
 export class UsuarioSearchComponent implements OnInit {
+  heroes$!: Observable<Usuario[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService) {}
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  usuarios: Usuario[] = []
 
   ngOnInit(): void {
+    this.usuarios = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.usuarioService.searchUsuarios(term)),
+    );
   }
+
 
 }
